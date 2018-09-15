5c0b0245ae175244e3e8bd1f7adcd0e111eb6c7c
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2018-09-15 16:18:17 +0300

    Add extra configuration

---
+++ config.js
@@ -0,0 +1,27 @@
+const baseConfig = {
+    ICON_CDN: '',
+}
+
+const configs = {
+    production: {
+        AUTH_API: '',
+        APP_URL: '',
+        API_BASE_URL: '',
+    },
+    development: {
+        AUTH_API: '',
+        APP_URL: '',
+        API_BASE_URL: '',
+    },
+    testing: {
+        AUTH_API: '',
+        APP_URL: '',
+        API_BASE_URL: '',
+    },
+    
+}
+
+const envConfig = configs[process.env.NODE_ENV];
+const config = {...baseConfig, ...envConfig}
+
+export default config;
