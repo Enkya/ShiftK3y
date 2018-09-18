b611a2916f9bc034c41dabf34472a3c1a36d62bd
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2018-09-18 15:01:13 +0300

    Create initial Route to Auth Component

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+[![CircleCI](https://circleci.com/gh/Enkya/ShiftK3y.svg?style=svg)](https://circleci.com/gh/Enkya/ShiftK3y)
 [![Maintainability](https://api.codeclimate.com/v1/badges/231e6487fbb20d514016/maintainability)](https://codeclimate.com/github/Enkya/ShiftK3y/maintainability)
 
 
---
+++ src/containers/Auth.jsx
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+/**
+ * @name Auth
+ * @summary renders the Authentication page
+ * @extends React.component
+ */
+class Auth extends Component {
+
+    render() {
+        return '<span>Test</span>';
+    }
+}
+
+export default withRouter(Auth);
