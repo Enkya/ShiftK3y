ff69a401f1f4b3169fccd6361ce9825648b9a5ea
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2018-10-03 18:29:56 +0300

    Add authentication helpers

--- src/helpers/authentication.js
+++ src/helpers/authentication.js
@@ -3,6 +3,8 @@
  * @summary checks if user role is amongst allowed roles
  * @return {boolean} whether user role is amongst allowed roles
  */
-export const hasAllowedRole = (userRoles, allowedRoles) => (
+const hasAllowedRole = (userRoles, allowedRoles) => (
   userRoles.some(role => allowedRoles.includes(role.toLowerCase()))
 );
+
+export default hasAllowedRole;
