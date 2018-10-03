48b2fa30d5ae07355601cd5e0c16a2d0ee65c6d1
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2018-10-03 12:48:02 +0300

    Add private route component

---
+++ src/components/routes/PrivateRoute.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import { hasAllowedRole } from '../../helpers/authentication';
+
+/**
+ * @name PrivateRoute
+ * @param {Object} props - React PropTypes
+ * @return Route component
+ */
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props => (
+      hasAllowedRole(rest.userRoles, rest.allowedPageRoles)
+        ? <Component {...props} />
+        : <Redirect to='/signin' />
+    )}
+  />
+);
+
+PrivateRoute.propTypes = {
+  component: PropTypes.func.isRequired,
+};
+
+export default PrivateRoute;
