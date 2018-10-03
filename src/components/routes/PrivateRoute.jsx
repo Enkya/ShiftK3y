9311d9426ce6b49d0186f4500bfff5122269e48b
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2018-10-03 18:31:33 +0300

    Add Redirect for pages not found

--- src/components/routes/PrivateRoute.jsx
+++ src/components/routes/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-import { hasAllowedRole } from '../../helpers/authentication';
+import hasAllowedRole from '../../helpers/authentication';
 
 /**
  * @name PrivateRoute
