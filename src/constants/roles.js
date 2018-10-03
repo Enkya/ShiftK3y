d5806db08403d04ee5ef259db148a0635f29b8bd
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2018-10-03 12:45:27 +0300

    Add user roles constants

---
+++ src/constants/roles.js
@@ -0,0 +1,17 @@
+// user roles
+export const LEGAL = 'LEGAL';
+export const ECI = 'ECI';
+export const HR = 'HR';
+export const SPECTRUM = 'SPECTRUM';
+export const TYPE_APPROVAL = 'TYPE_APPROVAL';
+
+// user groups
+export const TECHNICAL = [ECI, SPECTRUM, TYPE_APPROVAL];
+
+export const ROLES = {
+  LEGAL,
+  ECI,
+  HR,
+  SPECTRUM,
+  TYPE_APPROVAL,
+};
