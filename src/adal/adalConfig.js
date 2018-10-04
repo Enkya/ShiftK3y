2321d9ebe4f170536ffbcfc6199420b4aed49f86
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2018-10-04 15:49:27 +0300

    Add support for ADAL sign in

--- src/adal/adalConfig.js
+++ src/adal/adalConfig.js
@@ -1,12 +1,17 @@
-import * as AuthenticationContext from 'adal-angular/lib';
+import AuthenticationContext_ from 'adal-angular/lib/adal';
+import config from '../config';
 
-window.config = {
+export const adalConfig = {
   tenant: process.env.TENANT || 'brucebigirwenkyahotmail.onmicrosoft.com',
-  clientId: process.env.CLIENT_ID || '',
+  clientId: process.env.OFFICE_CLIENT_ID || '1bb2787b-90de-4d3f-9b65-523fe99b9cfe',
+  redirectUri: config.API_BASE_URL || 'http://localhost:3000/',
+  localLoginUrl: config.APP_URL,
+  popUp: true,
   endpoints: {
     api: '14d71d65-f596-4eae-be30-27f079bf8d4b',
   },
   cacheLocation: 'localStorage',
 };
 
-export const authContext = new AuthenticationContext(config);
+const AuthenticationContext = AuthenticationContext_;
+export const authContext = () => new AuthenticationContext(adalConfig);
--- src/config.js
+++ src/config.js
@@ -4,19 +4,19 @@ const baseConfig = {
 
 const configs = {
   production: {
-    AUTH_API: '',
-    APP_URL: '',
-    API_BASE_URL: '',
+    AUTH_API: process.env.AUTH_API || '',
+    APP_URL: process.env.APP_URL || '',
+    API_BASE_URL: process.env.API_BASE_URL || '',
   },
   development: {
-    AUTH_API: '',
-    APP_URL: '',
-    API_BASE_URL: '',
+    AUTH_API: process.env.AUTH_API || '',
+    APP_URL: process.env.APP_URL || '',
+    API_BASE_URL: process.env.API_BASE_URL || '',
   },
   testing: {
-    AUTH_API: '',
-    APP_URL: '',
-    API_BASE_URL: '',
+    AUTH_API: process.env.TESTING_AUTH_API || '',
+    APP_URL: process.env.TESTING_APP_URL || '',
+    API_BASE_URL: process.env.TESTING_API_BASE_URL || '',
   },
 
 };
