f592f3980481c6921903cce3c33ae2978a31d8b5
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2018-10-05 09:07:27 +0300

    Redesign Authentication page

--- src/containers/Auth.jsx
+++ src/containers/Auth.jsx
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-// import logo from '../assets/images/ucclogo.png';
+import logo from '../assets/images/ucclogo.png';
 import { authContext } from '../adal/adalConfig';
 
 /**
@@ -38,82 +38,39 @@ class Auth extends Component {
       const { signInError } = this.state;
       return (
         <Fragment>
-          <header className='signInHeader'>
-            <div className='signInPageContent'>
+          <header className='header'>
+            <div className='brand-box'>
               <div className='logo'>
-                {/* <img className='logo__img' src={logo} alt='UCC logo' /> */}
-                {/* <span className='logo__text'>UCC</span> */}
+                <img className='logo__img' src={logo} alt='UCC logo' />
               </div>
+            </div>
+            <div className='text-box'>
+              <h1 className='heading-primary'>
+                <span className='heading-primary-main'>SHIFTK3Y</span>
+                <span className='heading-primary-sub'>A UCC records management system</span>
+              </h1>
               <button
                 type='button'
-                className='signInButton'
+                className='btn btn-white btn-animated'
                 aria-label='Sign in with your microsoft or google account'
                 onClick={this.handleClick}
               >
                 <div className='signInButton__logo' />
                 <span>
-                  Sign in with your Microsoft or Google account
+                  Sign in with your UCC account
                 </span>
               </button>
-            </div>
-          </header>
-          <main className='signInPageContent signInPageContent--topAlignment'>
-            <div className='signInPageLeftContent'>
-              <p className='promo'>
-                {'Track application compliance reports.'}
-              </p>
               {
                 signInError
                   ? (
                     <span className='signInError'>
-                      <span className='signInError__message'>You must sign in with an Andela account</span>
+                      <span className='signInError__message'>You must sign in with your UCC account</span>
                     </span>
                   )
                   : null
               }
             </div>
-            <div className='signInPageRightContent'>
-              <h1 className='featuresTitle'>Features</h1>
-              <div className='features'>
-                <div className='feature'>
-                  <div className='feature__icon' />
-                  <div className='feature__info'>
-                    <h2 className='feature__name'>Log appliance report information</h2>
-                    <p className='feature__description'>
-                      Say goodbye to using spreadsheets for logging appliance data.
-                    </p>
-                  </div>
-                </div>
-                <div className='feature'>
-                  <div className='feature__icon' />
-                  <div className='feature__info'>
-                    <h2 className='feature__name'>Track compliance</h2>
-                    <p className='feature__description'>
-                      Easily sift through hundreds of records to find necessary information
-                    </p>
-                  </div>
-                </div>
-                <div className='feature'>
-                  <div className='feature__icon' />
-                  <div className='feature__info'>
-                    <h2 className='feature__name'>Report Generation</h2>
-                    <p className='feature__description'>
-                      Generate PDF reports from the data you upload
-                    </p>
-                  </div>
-                </div>
-                <div className='feature'>
-                  <div className='feature__icon' />
-                  <div className='feature__info'>
-                    <h2 className='feature__name'>Email Notifications</h2>
-                    <p className='feature__description'>
-                      Get notified when a record you are associated with gets updated
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </main>
+          </header>
         </Fragment>
       );
     }
