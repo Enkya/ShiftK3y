3eb1dc09eed3443359c76c1f89805f3bab4a09d0
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2018-11-04 21:23:17 +0300

    Add other section placeholders

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -62,7 +62,7 @@ const Home = () => (
         <div className='content'>
           <div className='content__header row-wrapper'>
             <div className='page__title'>
-              Licensing
+              Category
             </div>
             <div className='sorter sub'>
               <div className='toggle btn__small'>Status</div>
@@ -209,7 +209,7 @@ const Home = () => (
                     </div>
                     <div className='Spectrum section'>
                       <div className='title'>
-                        <span>Spectrum</span>
+                        <span>Spectrum Compliance</span>
                       </div>
                       <div className='details'>
                         <div className='band'>
@@ -317,14 +317,285 @@ const Home = () => (
                           <input type='text' id='authorizedByFirstName' placeholder='First Name'/>
                           <input type='text' id='authorizedByLastName' placeholder='Last Name'/>
                         </div>
-                        <div className='fullReport'>
-                          <label htmlFor='fullReport'>
+                        <div className='spectrum-fullReport'>
+                          <label htmlFor='spectrum-fullReport'>
                             Full Report
                           </label>
-                          <input type='file' id='fullReport' />
+                          <input type='file' id='spectrum-fullReport' />
                         </div>
                       </div>
                     </div>
+                    <div className='typeApproval section'>
+                      <div className='title'>
+                        <span>Type Approval</span>
+                      </div>
+                      <div className='details'>
+                        {/* TBD */}
+                        <div className='status'>
+                          <label htmlFor='status'>
+                            Status
+                          </label>
+                          <select name='status' className='value' id='status'>
+                            <option value='Approved'>Approved</option>
+                            <option value='Rejected'>Rejected</option>
+                          </select>
+                        </div>
+                        <div className='approveRejectDate'>
+                          <label htmlFor='approveRejectDate'>
+                            Approval/Rejection Date
+                          </label>
+                          <input type='date' id='approveRejectDate'/>
+                        </div>
+                        <div className='TAUniqueID'>
+                          <label htmlFor='TAUniqueID'>
+                            TA Unique ID
+                          </label>
+                          <input type='text' id='TAUniqueID'/>
+                        </div>
+                        <div className='equipmentCategory'>
+                          <label htmlFor='equipmentCategory'>
+                            Equipment Category
+                          </label>
+                          <select name='equipmentCategory' className='value' id='equipmentCategory'>
+                            <option value='STB'>STB</option>
+                            <option value='IDTV'>IDTV</option>
+                          </select>
+                        </div>
+                        <div className='equipmentName'>
+                          <label htmlFor='equipmentName'>
+                            Equipment Name
+                          </label>
+                          <input type='text' id='equipmentName'/>
+                        </div>
+                        <div className='equipmentModel'>
+                          <label htmlFor='equipmentModel'>
+                            Equipment Model
+                          </label>
+                          <input type='text' id='equipmentModel'/>
+                        </div>
+                        <div className='equipmentDesc'>
+                          <label htmlFor='equipmentDesc'>
+                            Equipment Description
+                          </label>
+                          <textarea rows='4' cols='50' id='equipmentDesc'/>
+                        </div>
+                        <div className='applicableStandards'>
+                          <label htmlFor='applicableStandards'>
+                            Applicable Standard(s)
+                          </label>
+                          <input type='text' id='applicableStandards'/>
+                        </div>
+                        <div className='applicantName'>
+                          <label htmlFor='applicantName'>
+                            Applicant Name
+                          </label>
+                          <input type='text' id='applicantName'/>
+                        </div>
+                        <div className='applicantAddress'>
+                          <label htmlFor='applicantAddress'>
+                            Applicant Address
+                          </label>
+                          <input type='text' id='applicant-addressline1'/>
+                          <input type='text' id='applicant-addressline2'/>
+                          <input type='text' id='applicant-city'/>
+                          <input type='text' id='applicant-state'/>
+                          <input type='number' id='applicant-postal'/>
+                          <input type='text' id='applicant-country'/>
+                        </div>
+                        <div className='typeApproval-fullReport'>
+                          <label htmlFor='typeApproval-fullReport'>
+                            Full Report
+                          </label>
+                          <input type='file' id='typeApproval-fullReport'/>
+                        </div>
+                        <div className='applicantName'>
+                          <label htmlFor='applicantName'>
+                            TA Certificate
+                          </label>
+                          <input type='text' id='applicantName'/>
+                        </div>
+                        <div className='taCertificate'>
+                          <label htmlFor='taCertificate'>
+                            TA Certificate
+                          </label>
+                          <input type='file' id='taCertificate' />
+                        </div>
+                        <div className='assessedBy'>
+                          <label htmlFor='assessedBy'>
+                            Assessed By
+                          </label>
+                          <input type='text' id='assessedByFirstName' placeholder='First Name'/>
+                          <input type='text' id='assessedByLastName' placeholder='Last Name'/>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='numbering section'>
+                      <div className='title'>
+                        <span>Numbering</span>
+                      </div>
+                      <div className='details'>
+                        {/* TBD */}
+                        <div className='serviceCategory'>
+                          <label htmlFor='serviceCategory'>
+                            Service Category
+                          </label>
+                          <select name='serviceCategory' id='serviceCategory'>
+                            <option value='Broadcasting'>Broadcasting</option>
+                            <option value='Telecom'>Telecommunications</option>
+                            <option value='Postal'>Postal</option>
+                          </select>
+                        </div>
+                        <div className='numberType'>
+                          <label htmlFor='numberType'>
+                            Number Type
+                          </label>
+                          <select name='numberType' id='numberType'>
+                            <option value='DVB IDs'>DVB IDs</option>
+                            <option value='LCN'>LCN</option>
+                            <option value='Network Codes'>Network Codes</option>
+                            <option value='ONIDs'>ONIDs</option>
+                            <option value='Short Code'>Short Code</option>
+                            <option value='Subscriber Numbers'>Subscriber Numbers</option>
+                          </select>
+                        </div>
+                        <div className='serviceName'>
+                          <label htmlFor='serviceName'>
+                            Service Name
+                          </label>
+                          <input type='text' id='serviceName'/>
+                        </div>
+                        <div className='applicableServiceType'>
+                          <label htmlFor='applicableServiceType'>
+                            Applicable Service Type
+                          </label>
+                          <select name='applicableServiceType' id='applicableServiceType'>
+                            <option value='Complimentary TV Services'>Complimentary TV Services</option>
+                            <option value='Content Provision Services'>Content Provision Services</option>
+                            <option value='Core TV Service'>Core TV Service</option>
+                            <option value='Emergency and Safety Services'>Emergency and Safety Services</option>
+                            <option value='Fixed Subscriber Services'>Fixed Subscriber Services</option>
+                            <option value='ISPC'>ISPC</option>
+                            <option value='MNC'>MNC</option>
+                            <option value='MNO Services'>MNO Services</option>
+                            <option value='Mobile Subscriber Services'>Mobile Subscriber Services</option>
+                            <option value='NSPC'>NSPC</option>
+                            <option value='Premium Subscriber Services'>Premium Subscriber Services</option>
+                            <option value='Special Services'>Special Services</option>
+                          </select>
+                        </div>
+                        <div className='serviceDescription'>
+                          <label htmlFor='serviceDescription'>
+                            Service Description
+                          </label>
+                          <textarea rows='5' cols='50' id='serviceDescription'/>
+                        </div>
+                        <div className='assignedRange'>
+                          <label htmlFor='assignedRange'>
+                            Assigned Range
+                          </label>
+                          <select name='assignedRange' id='assignedRange'>
+                            <option value='Choice 1'>Choice 1</option>
+                            <option value='Choice 2'>Choice 2</option>
+                            <option value='Choice 3'>Choice 3</option>
+                          </select>
+                        </div>
+                        <div className='assignedNumber'>
+                          <label htmlFor='assignedNumber'>
+                            Assigned Number
+                          </label>
+                          <select name='assignedNumber' id='assignedNumber'>
+                            <option value='Choice 1'>Choice 1</option>
+                            <option value='Choice 2'>Choice 2</option>
+                            <option value='Choice 3'>Choice 3</option>
+                            <option value='Choice 4'>Choice 4</option>
+                          </select>
+                        </div>
+                        <div className='numberAssignDate'>
+                          <label htmlFor='numberAssignDate'>
+                            Assignment Date
+                          </label>
+                          <input type='date' id='numberAssignDate' />
+                        </div>
+                        <div className='numberingAssignedBy'>
+                          <label htmlFor='numberingAssignedBy'>
+                            Assigned By
+                          </label>
+                          <input type='text' id='numberingAssignedByFirstName' placeholder='First Name'/>
+                          <input type='text' id='numberingAssignedByLastName' placeholder='Last Name'/>
+                        </div>
+                        <div className='lastAuthorizationRenewalDate'>
+                          <label htmlFor='lastAuthorizationRenewalDate'>
+                            Last Authorization Renewal Date
+                          </label>
+                          <input type='date' id='lastAuthorizationRenewalDate' />
+                        </div>
+                        <div className='complianceStatus'>
+                          <label htmlFor='complianceStatus'>
+                            Compliance Status
+                          </label>
+                          <select name='complianceStatus' id='complianceStatus'>
+                            <option value='Compliant'>Compliant</option>
+                            <option value='Not Compliant'>Not Compliant</option>
+                          </select>
+                        </div>
+                        <div className='numberingNotes'>
+                          <label htmlFor='numberingNotes'>
+                            Notes
+                          </label>
+                          <textarea rows='5' cols='50' id='numberingNotes'/>
+                        </div>
+                        <div className='numberingRecommendations'>
+                          <label htmlFor='numberingRecommendations'>
+                            Recommendations
+                          </label>
+                          <textarea rows='5' cols='50' id='numberingRecommendations'/>
+                        </div>
+                        <div className='numbering-fullReport'>
+                          <label htmlFor='numbering-fullReport'>
+                            Full Report
+                          </label>
+                          <input type='file' id='numbering-fullReport' />
+                        </div>
+                        <div className='lastUpdated'>
+                          <label htmlFor='lastUpdated'>
+                            Last Updated
+                          </label>
+                          <input type='date' id='lastUpdated' />
+                        </div>
+                        <div className='lastUpdatedBy'>
+                          <label htmlFor='lastUpdatedBy'>
+                            Last Updated By
+                          </label>
+                          <input type='text' id='lastUpdatedByFirstName' placeholder='First Name'/>
+                          <input type='text' id='lastUpdatedByLastName' placeholder='Last Name'/>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='broadcasting section'>
+                      <div className='title'>
+                        <span>Technical Broadcasting</span>
+                      </div>
+                      <div className='details'>
+                      {/* TBD */}
+                      </div>
+                    </div>
+                    <div className='postal section'>
+                      <div className='title'>
+                        <span>Technical Postal</span>
+                      </div>
+                      <div className='details'>
+                        
+                      </div>
+                    </div>
+                    <div className='telecom section'>
+                      <div className='title'>
+                        <span>Technical Telecom</span>
+                      </div>
+                      <div className='details'>
+                      {/* TBD */}
+                      </div>
+                    </div>
+
                   </div>
                 </form>
               </div>
