04027602183d208cdb3e84b37ceeb9856d791e40
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2019-01-21 14:48:57 +0300

    Add action types for entries

--- src/types/index.js
+++ src/types/index.js
@@ -9,3 +9,22 @@ export const FETCH_USER_PROFILE_FAILURE = 'FETCH_USER_PROFILE_FAILURE';
 // item types
 export const FETCH_ITEMS = 'FETCH_ITEM';
 export const SAVE_ITEM = 'SAVE_ITEM';
+
+// fetch entries types
+export const FETCH_ENTRIES_REQUEST = 'FETCH_ENTRIES_REQUEST';
+export const FETCH_ENTRIES_SUCCESS = 'FETCH_ENTRIES_SUCCESS';
+export const FETCH_ENTRIES_FAILURE = 'FETCH_ENTRIES_FAILURE';
+
+// create entry action types
+export const CREATE_ENTRY_REQUEST = 'CREATE_ENTRY_REQUEST';
+export const CREATE_ENTRY_SUCCESS = 'CREATE_ENTRY_SUCCESS';
+export const CREATE_ENTRY_FAILURE = 'CREATE_ENTRY_FAILURE';
+
+// create entry action types
+export const DELETE_ENTRY_REQUEST = 'DELETE_ENTRY_REQUEST';
+export const DELETE_ENTRY_SUCCESS = 'DELETE_ENTRY_SUCCESS';
+export const DELETE_ENTRY_FAILURE = 'DELETE_ENTRY_FAILURE';
+// edit entry action types
+export const EDIT_ENTRY_REQUEST = 'EDIT_ENTRY_REQUEST';
+export const EDIT_ENTRY_SUCCESS = 'EDIT_ENTRY_SUCCESS';
+export const EDIT_ENTRY_FAILURE = 'EDIT_ENTRY_FAILURE';
