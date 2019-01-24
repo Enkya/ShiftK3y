4fef7bbf49dd1a3175fd1f3529c50bcc3e297017
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2019-01-24 11:40:34 +0300

    Add Release Check for Features

---
+++ src/helpers/release.js
@@ -0,0 +1,10 @@
+const NOT_READY = [
+  'ContentHeader',
+  'entryCounter',
+  'PersonalActions',
+  'API_ENDPOINT',
+];
+
+const IS_NOT_READY = item => (NOT_READY.includes(item));
+
+export default IS_NOT_READY;
