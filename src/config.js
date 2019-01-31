848da2764a6fea59946c1114d69b8e9ed6a340b1
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2019-01-31 16:40:29 +0300

    Update configuration

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ const configs = {
   development: {
     AUTH_API: process.env.AUTH_API || '',
     APP_URL: process.env.APP_URL || '',
-    API_BASE_URL: process.env.API_BASE_URL || 'http://127.0.0.1:5000/api/v1',
+    API_BASE_URL: process.env.API_BASE_URL || 'https://shiftk3yapi.herokuapp.com/api/v1',
   },
   testing: {
     AUTH_API: process.env.TESTING_AUTH_API || '',
