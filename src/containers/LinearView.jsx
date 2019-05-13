1162847aaa89b888755cab813c2176a3bb4b10a9
Bruce Bigirwenkya <brucebigirwenkya@gmail.com>
2019-05-13 14:52:30 +0300

    Make linearview stateful

--- src/containers/LinearView.jsx
+++ src/containers/LinearView.jsx
@@ -1,54 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import ErrorMessage from '../common/ErrorMessage';
 
-const createItems = items => (
-  items.map(item => (
-    <div className='card' key={item.id}>
-      <div className='avatar'>
-        <div className='user__image' />
-      </div>
-      <div className='summary'>
-        <div className='name'>
-          {item.name}
-        </div>
-        <div className='category'>
-          {item.location}
-        </div>
-        <div className='services'>
-          {item.postal}
-        </div>
-        <div className='status'>
-          {item.country}
-        </div>
-        <div className='resource'>
-          {item.tech_person_name}
-        </div>
-      </div>
-    </div>
-  ))
-);
 
-const LinearView = ({ items }) => {
-  let LinearViewHtml = '';
-  if (items.length) {
-    LinearViewHtml = (
-      <div className='linear-view'>
-        {createItems(items)}
+class LinearView extends Component {
+  static defaultProps = {
+    items: [],
+  };
+
+  static propTypes = {
+    items: PropTypes.arrayOf(PropTypes.shape({})),
+  };
+
+  createItems = items => (
+    items.map(item => (
+      <div
+        className='card'
+        key={item.id}
+        onClick={this.renderPreview}
+        onKeyPress={this.onKeyPressHandler}
+        role='button'
+        tabIndex='0'
+      >
+        <div className='avatar'>
+          <div className='user__image' />
+        </div>
+        <div className='summary'>
+          <div className='name'>
+            <Link to='/'>{item.name}</Link>
+          </div>
+          <div className='category'>
+            {item.location}
+          </div>
+          <div className='services'>
+            {item.postal}
+          </div>
+          <div className='status'>
+            {item.country}
+          </div>
+          <div className='resource'>
+            {item.tech_person_name}
+          </div>
+        </div>
       </div>
-    );
-  } else {
-    LinearViewHtml = (<ErrorMessage message='There are no resources at the moment' />);
+    ))
+  );
+
+  renderPreview = (event) => {
+    event.preventDefault();
+    console.log(event);
+    console.log('test');
+  };
+
+  onKeyPressHandler = (event) => {
+    event.preventDefault();
+    console.log(event);
+  };
+
+  render() {
+    const { items } = this.props;
+    let LinearViewHtml = '';
+    if (items.length) {
+      LinearViewHtml = (
+        <div className='linear-view'>
+          {this.createItems(items)}
+        </div>
+      );
+    } else {
+      LinearViewHtml = (<ErrorMessage message='There are no resources at the moment' />);
+    }
+    return LinearViewHtml;
   }
-  return LinearViewHtml;
-};
-
-LinearView.defaultProps = {
-  items: [],
-};
-
-LinearView.propTypes = {
-  items: PropTypes.arrayOf(PropTypes.shape({})),
-};
+}
 
 export default LinearView;
